de7554a44ca2320d8ca7a833f8e3f8a45d923e62
Muhammad zaid <98674411+mdzaid31@users.noreply.github.com>
2024-06-08 18:21:11 +0400

    Delete compass_backend directory

--- compass_backend/schema.py
+++
@@ -1,7 +0,0 @@
-user_profile_schema = {
-    'username': 'johndoe',
-    'password_hash': 'hashed_password',
-    'fullname': 'John Doe',
-    'compassxp_points': 0,
-    'created_at': '2023-06-08T12:00:00Z'
-}
